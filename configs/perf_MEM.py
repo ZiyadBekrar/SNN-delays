9385e37241c986fd2cbe277b58ab49b88e7b973f
Ziyad CS <ziyad.bekrar@student-cs.fr>
2026-09-09 18:35:34 +0200

    Scale epoch and num_samples

--- configs/perf_MEM.py
+++ configs/perf_MEM.py
@@ -13,13 +13,13 @@ class Config:
     seed = 0
     dataset_seed = 0
     task_type = "temporal"
-    num_samples = 256
+    num_samples = 5000
     input_size = 16
     time_window = 32
     output_size = 4
     input_gain = 1.0
     hidden_layers = [64]
-    epochs = 200
+    epochs = 3000
     batch_size = 32
     num_workers = 0
     cpu_threads = 1
